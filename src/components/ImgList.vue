ae8d4a17d136b1df0cf3f4b5c2f1bf8ca1bd8aeb
zengwei <934725536@qq.com>
2019-10-24 16:22:15 +0800

    还原

--- src/components/ImgList.vue
+++ src/components/ImgList.vue
@@ -10,7 +10,7 @@
   bottomKey: String // 左下角补充信息所对应的字段 默认 'bottom'
  */
 <template>
-  <el-row :gutter="15" class="image-card-row" @mouseleave="leave">
+  <el-row :gutter="15" class="image-card-row" @mouseleave="showInfo = -1">
     <el-col :span="6" v-for="(item, index) in list" :key="`${index}图片列表`">
       <el-card shadow="hover">
         <el-image fit="cover" @mouseenter.stop="showInfo = index" src="http://photocdn.sohu.com/20120625/Img346436473.jpg"/>
@@ -19,7 +19,7 @@
           <div
             v-show="(showInfo == index && trigger == 'hover') || trigger == 'always'"
             class="hover-wrap mouse-class"
-            @mouseleave="leave">
+            @mouseleave="showInfo = -1">
             <p class="top margin-b-0"><small>{{ item[statusKey] ? item[statusKey] : '&nbsp;' }}</small></p>
             <h4 class="margin-b-0">{{ item[titleKey] ? item[titleKey] : "这是默认标题" }}</h4>
             <p class="bottom"><small>{{ item[bottomKey] ? item[bottomKey] : '&nbsp;' }}</small></p>
