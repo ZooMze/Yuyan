2390688c952b80ec5919db193e530256adf6673d
ZooMze <1057696018@qq.com>
2019-10-24 17:24:33 +0800

    解决 图片列表组件 快速滑动浮动失效的问题

--- src/components/ImgList.vue
+++ src/components/ImgList.vue
@@ -10,30 +10,32 @@
   bottomKey: String // 左下角补充信息所对应的字段 默认 'bottom'
  */
 <template>
-  <el-row :gutter="15" class="image-card-row" @mouseleave="showInfo = -1">
+  <el-row :gutter="15" class="image-card-row">
     <el-col :span="6" v-for="(item, index) in list" :key="`${index}图片列表`">
       <el-card shadow="hover">
-        <el-image fit="cover" @mouseenter.stop="showInfo = index" src="http://photocdn.sohu.com/20120625/Img346436473.jpg"/>
-        <!-- 完整浮层内容 -->
-        <transition name="slide-up">
-          <div
-            v-show="(showInfo == index && trigger == 'hover') || trigger == 'always'"
-            class="hover-wrap mouse-class"
-            @mouseleave="showInfo = -1">
-            <p class="top margin-b-0"><small>{{ item[statusKey] ? item[statusKey] : '&nbsp;' }}</small></p>
-            <h4 class="margin-b-0">{{ item[titleKey] ? item[titleKey] : "这是默认标题" }}</h4>
-            <p class="bottom"><small>{{ item[bottomKey] ? item[bottomKey] : '&nbsp;' }}</small></p>
-          </div>
-        </transition>
-        <!-- 底部缩略标题 -->
-        <transition name="slide-down">
-          <div
-            v-show="showInfo != index && trigger == 'hover'"
-            class="brief"
-            @mouseenter.stop="showInfo = index">
-            <p>{{ item[titleKey] ? item[titleKey] : "这是默认标题" }}</p>
-          </div>
-        </transition>
+        <div @mouseenter.stop="showInfo = index" @mouseleave.stop
+        ="leaveCard">
+          <el-image fit="cover"  src="http://photocdn.sohu.com/20120625/Img346436473.jpg"/>
+          <!-- 完整浮层内容 -->
+          <transition name="slide-up">
+            <div
+              v-show="(showInfo == index && trigger == 'hover') || trigger == 'always'"
+              class="hover-wrap mouse-class">
+              <p class="top margin-b-0"><small>{{ item[statusKey] ? item[statusKey] : '&nbsp;' }}</small></p>
+              <h4 class="margin-b-0">{{ item[titleKey] ? item[titleKey] : "这是默认标题" }}</h4>
+              <p class="bottom"><small>{{ item[bottomKey] ? item[bottomKey] : '&nbsp;' }}</small></p>
+            </div>
+          </transition>
+          <!-- 底部缩略标题 -->
+          <transition name="slide-down">
+            <div
+              v-show="showInfo != index && trigger == 'hover'"
+              class="brief"
+              @mouseenter.stop="showInfo = index">
+              <p>{{ item[titleKey] ? item[titleKey] : "这是默认标题" }}</p>
+            </div>
+          </transition>
+        </div>
       </el-card>
     </el-col>
   </el-row>
@@ -75,10 +77,9 @@
       }
     },
     methods: {
-      leave:function(){
-        setTimeout(_ => {
-          this.showInfo = -1
-        },50)
+      leaveCard () {
+        
+        this.showInfo = -1
       }
     }
   }
