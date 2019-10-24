f0cf994dcbb89f0a2a6e1b2c68945f1b7fe8268c
ZooMze <1057696018@qq.com>
2019-10-24 10:37:00 +0800

    更新样式

--- src/components/ImgList.vue
+++ src/components/ImgList.vue
@@ -1,7 +1,7 @@
 /**
  * 附带图片背景的预览组件
- 快速对el-tag 进行增删改操作 , 使用v-model回调
- by ZooMze 2019/10/15
+ 常用的图片文字展示列表 , 用在首页工作台等
+ by ZooMze 2019/10/16
  参数: 
   list: Array, //列表数据
   trigger: String, // 'hover' 浮动显示(默认) 'always' 始终显示
@@ -16,7 +16,10 @@
         <el-image fit="cover" @mouseenter.stop="showInfo = index" src="http://photocdn.sohu.com/20120625/Img346436473.jpg"/>
         <!-- 完整浮层内容 -->
         <transition name="slide-up">
-          <div v-if="(showInfo == index && trigger == 'hover') || trigger == 'always'" @mouseleave="showInfo = -1" class="hover-wrap">
+          <div
+            v-show="(showInfo == index && trigger == 'hover') || trigger == 'always'"
+            class="hover-wrap"
+            @mouseleave="showInfo = -1">
             <p class="top margin-b-0"><small>{{ item[statusKey] ? item[statusKey] : '&nbsp;' }}</small></p>
             <h4 class="margin-b-0">{{ item[titleKey] ? item[titleKey] : "这是默认标题" }}</h4>
             <p class="bottom"><small>{{ item[bottomKey] ? item[bottomKey] : '&nbsp;' }}</small></p>
@@ -24,7 +27,10 @@
         </transition>
         <!-- 底部缩略标题 -->
         <transition name="slide-down">
-          <div v-if="showInfo != index && trigger == 'hover'" @mouseenter.stop="showInfo = index" class="brief">
+          <div
+            v-show="showInfo != index && trigger == 'hover'"
+            class="brief"
+            @mouseenter.stop="showInfo = index">
             <p>{{ item[titleKey] ? item[titleKey] : "这是默认标题" }}</p>
           </div>
         </transition>
@@ -54,6 +60,13 @@
       bottomKey: {
         type: String,
         default: 'bottom'
+      }
+    },
+    watch: {
+      showInfo: {
+        handler(newV, oldV) {
+          console.log(oldV, newV)
+        }
       }
     },
     data() {
